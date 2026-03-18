37749da29fee89d7b6eb9bea64b9d4e805abb615
CaticBranko <catibranko7@yahoo.com>
2026-03-18 21:34:48 +0100

    Initial javascript code, added function outlines

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("ZMAJEEE")
+
+function getComputerChoice () {
+
+}
+
+function getHumanChoice () {
+    
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice(); 
+
+playRound (humanSelection, computerSelection)
